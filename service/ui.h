252955a4289889e8e29d70423460fae2f4d4d942
your name <you@example.com>
2026-08-23 16:24:40 +0700

    Update ui.h

---
+++ service/ui.h
@@ -0,0 +1,32 @@
+#ifndef _UI_H_
+#define _UI_H_
+
+#include "stdint.h"
+
+typedef enum {
+	UI_PAGE_WAITING = 0,
+	UI_PAGE_MENU,
+	UI_PAGE_BATTLE,
+	UI_PAGE_ENDLESS,
+	UI_PAGE_SETTING,
+} UiPage;
+
+extern uint8_t uid[4];
+extern uint8_t card;
+extern UiPage  ui_page;
+extern int8_t  current_player;
+extern uint8_t select_mode;
+
+void Menu_Game(void);
+void Waiting_Display(void);
+void Check_Card(void);
+void Ui_PollWaitingScreen(void);
+void Ui_Select_Mode(uint8_t select_mode);
+void Ui_Deselect_Mode(uint8_t deselect_mode);
+void Ui_Display_Battle_Stat(void);
+void Ui_Endless_Page(void);
+void Ui_Setting_Page(void);
+void Ui_Confirm_Mode(uint8_t mode);
+void Ui_Move_Mode(uint8_t *sl_mode);
+void Ui_Battle_Page(void);
+#endif // _UI_H_
